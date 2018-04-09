7183e835f3a7c803a823c8954913931750bde1bc
Kim Schulz <kim@schulz.dk>
2018-04-09 22:50:13 +0200

    support for both xrandr and xfconf-query
    Extended script to support using both xrandr and xfconf-query.
    If you use XFCE then xfconf-query works faster and more smooth.
    For the rest of you - use xrandr

--- gpd-toggle-dpi.sh
+++ gpd-toggle-dpi.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-
-CURRENT="$(xrandr --current|grep "primary 1229")"
-
+if [[ "x$1" == "x" ]]; then
+	echo "Please specify method to use (xrandr or xfce)"
+	exit 1
+fi
+if [ ! -f "~/.gdp-toggle-dpi-xrandr" ]; then
+	touch ~/.gdp-toggle-dpi-xrandr
+fi
+if [[ "$1" == "xrandr" ]]; then 
+	echo "using xrandr"
+	FULL="1x1"
+	HALF="0.64x0.64"
+	CURRENT="$(cat ~/.gpd-toggle-dpi-xrandr|grep "${HALF}")"
+	DISP="$(xrandr -q|grep primary|cut -f 1 -d " ")"
+	COMMAND="xrandr --output ${DISP} --scale "
+elif [[ "$1" == "xfce" ]]; then
+	FULL="100"
+	HALF="150"
+	CURRENT="$(xfconf-query -c xsettings -p /Xft/DPI|grep "${HALF}")"
+	COMMAND="xfconf-query -c xsettings -p /Xft/DPI -s "
+else
+	echo "Please specify method to use (xrandr or xfce)"
+	exit 1
+fi	
+	
 if [[ "x${CURRENT}" == "x" ]]; then
 	echo -n "Switching to low res..."
-	xrandr --output DSI1 --scale 0.64x0.64
+	echo "${HALF}" > $HOME/.gpd-toggle-dpi-xrandr
+	${COMMAND} ${HALF}
 	echo "done"
 else
 	echo -n "switching to high res..."
-	xrandr --output DSI1 --scale 1x1
+	${COMMAND} ${FULL}
+	echo "${FULL}" > ~/.gpd-toggle-dpi-xrandr
 	echo "done"
 fi
